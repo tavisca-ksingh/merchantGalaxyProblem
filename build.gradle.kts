1fe27af9e3ad46fd899fbe19cc443aafce8ae79f
tavisca-ksingh <ksingh@tavisca.com>
2019-08-14 14:15:32 +0530

    Test cases juint jupiter added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+
+
 plugins {
     java
 }
@@ -9,10 +11,42 @@ repositories {
     mavenCentral()
 }
 
+
+
 dependencies {
-    testCompile("junit", "junit", "4.12")
+    implementation("org.json:json:20180813")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.0")
 }
+
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
+    
+}
+tasks.register<Test>("hidden-tests")
+tasks.named<Test>("test") {
+    dependsOn("cleanTest")
+    useJUnitPlatform {
+        includeEngines("junit-jupiter")
+        excludeTags("hidden")
+    }
+
+    testLogging {
+        showExceptions = true
+        events("passed", "skipped", "failed")
+    }
+}
+
+
+tasks.named<Test>("hidden-tests") {
+    useJUnitPlatform {
+        includeEngines("junit-jupiter")
+        includeTags("hidden")
+    }
+
+    testLogging {
+        showExceptions = true
+        events("passed", "skipped", "failed")
+    }
 }
--- src/test/java/com/tavisca/workshop/mgttg/InputParserTest.java
+++ src/test/java/com/tavisca/workshop/mgttg/InputParserTest.java
@@ -1,7 +1,9 @@
 package com.tavisca.workshop.mgttg;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class InputParserTest {
     WordToRomanParser wordToRomanParser= new WordToRomanParser();
